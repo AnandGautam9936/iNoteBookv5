f2560ec1d11bc82e6958b588d05de3d62f397938
Anand Gautam <Anand.Gautam@landisgyr.com>
2025-04-05 15:54:31 +0530

    first commit Identification: iNoteBookV5
    Change-Id: I06e53485cb209b4b89b6cfefae44d0673ad1a5b6

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const connectToMongo = require('./db');
+const path = require("path")
 
 connectToMongo();
 
@@ -6,16 +7,18 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 const port = 5000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+const _dirname = path.resolve(); 
 
 app.use(cors())
 app.use(express.json()); //This is a middleware which is used to print the body
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
+
+app.use(express.static(path.join(_dirname,"/frontend/build")));
+app.get('*', (_, res)=>{
+  res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`iNotebook BackEnd is listening on port ${port}`)
